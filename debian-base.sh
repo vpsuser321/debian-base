a17de88bacfa4da0b49c344185a7497c284e77d0
vpsuser321 <94027222+vpsuser321@users.noreply.github.com>
2021-11-10 02:13:28 +0000

    Update debian-base.sh

--- debian-base.sh
+++ debian-base.sh
@@ -1,5 +1,5 @@
 #!bin/sh
 apt update -y && apt upgrade -y && apt dist-upgrade
-/etc/apt/sources.list
-apt update -y && apt full-upgrade -y
 apt install htop git nano wget curl ufw neofetch gcc net-tools man
+wget https://raw.githubusercontent.com/vpsuser321/debian-base/main/files/sources.list /etc/apt/sources.list
+apt update -y && apt full-upgrade -y

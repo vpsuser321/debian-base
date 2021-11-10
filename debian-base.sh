f99df083b4e07f80cb7924e7357d5d3516421ae2
vpsuser321 <94027222+vpsuser321@users.noreply.github.com>
2021-11-10 02:11:55 +0000

    Create debian-base.sh

---
+++ debian-base.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+apt update -y && apt upgrade -y && apt dist-upgrade
+/etc/apt/sources.list
+apt update -y && apt full-upgrade -y
+apt install htop git nano wget curl ufw neofetch gcc net-tools man
